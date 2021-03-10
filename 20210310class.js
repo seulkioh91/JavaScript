1eb675fdafb50177c49821a526d6ef5149717994
seulkioh91 <78770899+seulkioh91@users.noreply.github.com>
2021-03-10 17:15:00 +0900

    20210310class

---
+++ 20210310class.js
@@ -0,0 +1,7 @@
+// var name = "홍길동";
+var name = prompt("이름을 입력해 주세요.")
+
+var msg = "당신의 이름은" + name+"입니다"+ name.length+"글자입니다"
+
+//console.log(msg); // 개발할 때는 경고 창을 띄우지 말고 콘솔에서 메시지를 확인
+alert(msg);
